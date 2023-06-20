5b55a656607b2e75ea36eca6e467778e94baaf8e
Andreu3012 <107481826+Andreu3012@users.noreply.github.com>
2023-06-19 22:27:26 -0600

    conexion.php

--- TiendaOnline/config/conexion.php
+++ TiendaOnline/config/conexion.php
@@ -5,5 +5,5 @@ session_start();
     $DB_PASSWORD=$_ENV["DB_PASSWORD"];
     $DB_NAME=$_ENV["DB_NAME"];
     $DB_PORT=$_ENV["DB_PORT"];
-    $db=mysqli_connect("$DB_HOST","$DB_USER","$DB_PASSWORD","$DB_NAME","$DB_PORT");
+    $conexion=mysqli_connect("$DB_HOST","$DB_USER","$DB_PASSWORD","$DB_NAME","$DB_PORT");
 ?>
