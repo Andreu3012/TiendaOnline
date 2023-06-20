d61d78d61527fd0039224834ce22dd5eecfddc30
Andreu3012 <107481826+Andreu3012@users.noreply.github.com>
2023-06-19 22:01:42 -0600

    conexion.php

--- TiendaOnline/config/conexion.php
+++ TiendaOnline/config/conexion.php
@@ -1,12 +1,9 @@
 <?php
-    $host = "localhost";
-    $user = "root";
-    $clave = "ars1709cyj";
-    $bd = "tienda";
-    $conexion = mysqli_connect($host,$user,$clave,$bd);
-    if (mysqli_connect_errno()){
-        echo "No se pudo conectar a la base de datos";
-        exit();
-    }
-    mysqli_select_db($conexion,$bd) or die("No se encuentra la base de datos");
-    mysqli_set_charset($conexion,"utf8");
+session_start();
+    $DB_HOST=$_ENV["DB_HOST];
+    $DB_USER=$_ENV["DB_USER];
+    $DB_PASSWORD=$_ENV["DB_PASSWORD];
+    $DB_NAME=$_ENV["DB_NAME];
+    $DB_PORT=$_ENV["DB_PORT];
+    $db=mysqli_connect("$DB_HOST","$DB_USER","$DB_PASSWORD","$DB_NAME","$DB_PORT");
+?>
